1f8f519c0c3fadce390862b56ba1492b8249a1c9
michaelbdai <michaelbdai@gmail.com>
2016-09-06 11:47:58 -0700

    fizzbuzz v2

--- js/FizzBuzz.js
+++ js/FizzBuzz.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
-  function fizzBuzz (num){
+  function fizzBuzz (num, var1, var2, text1, text2){
     var arrRes = []
     for (var i = 1; i <= num; i ++){
-      if (i % 15 === 0) {
-        arrRes.push("fizz buzz");
-      }else if(i % 3 === 0){
-        arrRes.push("fizz");
-      }else if (i % 5 === 0){
-        arrRes.push("buzz")
+      if (i % (var1 * var2) === 0) {
+        arrRes.push(text1 + text2);
+      }else if(i % var1 === 0){
+        arrRes.push(text1);
+      }else if (i % var2 === 0){
+        arrRes.push(text2)
       }else{
         arrRes.push(i);
       }
     }
     return arrRes;
   }
-  $("body").text('fizzBuzz 30 is ->' + fizzBuzz(30));
+  function getInput(callBack){
+    var num = prompt("How many number do you want to count?","1000");
+    var var1 = parseInt(prompt("What is 'fizz value'?", 3));
+    var var2 = parseInt(prompt("What is 'buzz value'?", 5));
+    var text1 = prompt("What is 'fizz text'?", "fizz");
+    var text2 = prompt("What is 'buzz text'?", "buzz");
+    return callBack(num, var1, var2, text1, text2);
+  }
+  $(".fizz1").text('fizzBuzz 30 is -> ' + fizzBuzz(30,3,5,"fizz","buzz"));
+  $(".fizz2").text('Try the new fizzBuzz is -> ' + getInput(fizzBuzz));
 });
