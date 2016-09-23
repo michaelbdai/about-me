9a4912893859df398e58d7d5d3fc5d23cc4a9e7d
michaelbdai <michaelbdai@gmail.com>
2016-09-23 01:25:25 -0700

    9/23

--- api-music/js/app.js
+++ api-music/js/app.js
@@ -14,27 +14,34 @@ var displayTracks = function(tracksInfo){
 	//pass tracksInfo object to html
   var result = $('.templates .tracks-result').clone();
 	var dispAlbName = result.find('.display-text1');
-	dispAlbName.text('Name: '+ tracksInfo.album.name);
 	var dispArtName = result.find('.display-text2');
+	var detailInfo = result.find('.display-text3');
+	var infoStr = '';
+
+	dispAlbName.text('Name: '+ tracksInfo.album.name);
 	dispArtName.text('Singer: ' + tracksInfo.artists[0].name);
 	result.attr('style','background-image: linear-gradient(rgba(0, 0, 0, 1) , rgba(0, 0, 0, 0.3)),url(\'' + tracksInfo.album.images[1].url +'\'); background-size: contain;' )
-	var detailInfo = result.find('.display-text3');
-
-	var infoStr = '';
-	infoStr += 'mp3-preview-url' + ':' + tracksInfo.preview_url + '_';
-	infoStr += 'id' + ':' + tracksInfo.id +'_';
-	infoStr += 'popularity' + ':' + tracksInfo.popularity +'_';
-	infoStr += 'external-url' + ':' + tracksInfo.external_urls.spotify +'_';
+	infoStr += 'mp3-preview-url' + '->' + tracksInfo.preview_url + '_';
+	infoStr += 'id' + '->' + tracksInfo.id +'_';
+	infoStr += 'popularity' + '->' + tracksInfo.popularity +'_';
+	infoStr += 'external-url' + '->' + tracksInfo.external_urls.spotify +'_';
+	infoStr += 'img-url' + '->' + tracksInfo.album.images[1].url;
 	detailInfo.text(infoStr);
 	return result;
 }
-var videoHtml = function(mp3Url,posterUrl){
-	var playerHtml = '<video src=\''
-	+ mp3Url
-	+ '\' controls poster=\''
-	+ posterUrl
-	+ '\'></video>';
-	return playerHtml
+var displaySpotifyDetails = function(songNameStr,singerNameStr,detailArr){
+	var details = $('.spotify-details').clone();
+	var dispVid = details.find('video');
+	var dispBas = details.find('.display-text1');
+	var dispPop = details.find('.display-text2')
+	var dispIcon = details.find('.icon');
+
+	dispVid.attr('src', detailArr[0].split('->')[1]);
+	dispVid.attr('poster', detailArr[4].split('->')[1]);
+	dispIcon.attr('href', detailArr[3].split('->')[1]);
+	dispBas.text(songNameStr + ' by ' + singerNameStr + '. ');
+	dispPop.text('Song popularity: ' + detailArr[2].split('->')[1] + '/100');
+  return details;
 }
 var searchSpotify = function(searchQuery, searchType, searchLimit){
 	var request = {
@@ -91,12 +98,19 @@ $(document).ready( function() {
 	event.preventDefault();
 		// $('.result').show();
 	$('.top-layer').show();
+	$('.detail-result').hide();
+	$('.result-body').hide();
+	$('.result').hide();
 	$('.search-spotify, nav .search-general').submit( function(e){
 		e.preventDefault();
 		$('.top-layer').hide();
+		$('.result-body').show();
 		$('.result').show();
+		$('.detail-result').hide();
+
 		// zero out results if previous search has run
 		// !!! need to change this later !!!
+		$('.spotify-wrapper').html('');
 		$('.result-body').html('');
     resetGlobalVar();
 		// get the value of the tags the user submitted
@@ -110,9 +124,11 @@ $(document).ready( function() {
 	$(document).on('click','.tracks-result',function(event){
 		var nameStr = $(this).find('.display-text1').text().split(': ', 2)[1];
 		var singerStr = $(this).find('.display-text2').text().split(': ', 2)[1];
+		var detailArr = $(this).find('.display-text3').text().split('_');
 		// console.log(nameStr);
 		// console.log(singerStr);
-// 9/21 ******************************************************************
+		console.log(detailArr);
+		$('.detail-result .spotify-wrapper').append(displaySpotifyDetails(nameStr,singerStr,detailArr))
 		getMM(nameStr,singerStr);
 		// getTK(nameStr,singerStr);
 		// getBT(nameStr,singerStr);
@@ -121,12 +137,13 @@ $(document).ready( function() {
 	})
 
 	$('.logo-small').on('click',function(){
+		$('.result-body').hide();
 		$('.result').hide();
+		$('.detail-result').hide();
 		$('.top-layer').show();
 	});
 });
 
-//****************************************************************************************************************
 var getMM = function(name,artist){
 	console.log(name + ' by ' + artist);
 	$.ajax({
@@ -178,6 +195,7 @@ var getTK = function(name,artist){
 			// console.log(result.Similar.Info[0]);
 		});
 }
+
 var getBT = function(name,artist){
 	var urlBandsintown1 = 'http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=spotifisearch&callback=?';
 	$.getJSON(urlBandsintown1,
