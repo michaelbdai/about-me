88dde6a5143072ba95deb73935b5018dac4f5acf
michaelbdai <michaelbdai@gmail.com>
2016-09-29 21:20:16 -0700

    9/29

--- api-music/js/app.js
+++ api-music/js/app.js
@@ -43,6 +43,110 @@ var displaySpotifyDetails = function(songNameStr,singerNameStr,detailArr){
 	dispPop.text('Song popularity: ' + detailArr[2].split('->')[1] + '/100');
   return details;
 }
+var displayLyrics = function(lyricsInformaiton){
+	var result = $('.templates .mm-details').clone();
+	var dispLyrics = result.find('.display-text1');
+	dispLyrics.html(lyricsInformaiton);
+	return result
+}
+var getMM = function(name,artist){
+	// console.log(name + ' by ' + artist);
+	$.ajax({
+
+			url: "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
+			//jsonp: false,
+			dataType: "jsonp",
+			jsonp: 'callback',
+			data: {
+							q_track: name,
+							q_artist: artist,
+							apikey: '33620d00ea8d8795ff4e49f0b4549d03',
+							format: 'jsonp',
+			}
+	}).done(function(result){
+		// console.log('searched musizmatch');
+		// console.log(result);
+		if(result.message.body.length === 0){
+			// console.log('no result from musizmatch');
+			return 'no result from musizmatch';
+		};
+		// console.log(result.message.body.length);
+		//console.log(result.message.body.lyrics.lyrics_id);
+		var lyricsBody = result.message.body.lyrics.lyrics_body;
+		lyricsBody = lyricsBody.split('\n').join('<br>');
+		//console.log(lyricsBody);
+		$('.extend-wrapper .mm-wrapper').append(displayLyrics(lyricsBody));
+	});
+};
+var displayTKResult = function(searchResult){
+	var result = $('.templates .tk-details').clone();
+	var dispText = result.find('.display-text1');
+	console.log(searchResult.details);
+	dispText.html(searchResult.details);
+	return result
+}
+var getTKRecursionCounter = 0;
+var getTK = function(keyword1,keyword2){
+	$.ajax({
+			url: "https://www.tastekid.com/api/similar",
+	    //jsonp: false,
+	    dataType: "jsonp",
+			jsonp: 'callback',
+
+	    data: {
+							k: '240254-BingDai-6SQWHIFJ',
+							q: keyword1,
+							info: 1,
+							type: 'music',
+							format: 'jsonp',
+	    }
+	}).done(function(result){
+		if(result.Similar.Info[0].Type === 'unknown'){
+			console.log('Cannot find information about ' + keyword1 + ' from tastekid');
+			if(getTKRecursionCounter === 0){
+				getTKRecursionCounter ++;
+				getTK(keyword2);
+			}
+			getTKRecursionCounter = 0;
+			return;
+		};
+		var detailsStr = '<p>' + result.Similar.Info[0].wTeaser + '</p>'
+		console.log(detailsStr);
+		detailsStr = detailsStr.replace(/([a-z0-9])\.([A-Z])/g ,'$1.</p><p>$2');
+		//(?!\s)  /(?=[a-z])\.(?!\s)/g
+		var searchResult = {
+			details : detailsStr,
+			wURL : result.Similar.Info[0].wUrl,
+			yURL : 'https://www.youtube.com/watch?v=' + result.Similar.Info[0].yID,
+		};
+		console.log(searchResult);
+		$('.extend-wrapper .tk-wrapper').append(displayTKResult(searchResult))
+	});
+}
+
+var getBT = function(name,artist){
+	var urlBandsintown1 = 'http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=spotifisearch&callback=?';
+	$.getJSON(urlBandsintown1,
+		function(result){
+			console.log('search bandsintown1')
+			console.log(result);
+			if(result.length === 0){
+				console.log('no result from bandsintown1');
+				return
+			};
+		})
+	var urlBandsintown2 = 'http://api.bandsintown.com/artists/'+ artist +'/events/search?format=json&app_id=spotifisearch&api_version=2.0&location=use_geoip&radius=150&callback=?'
+	$.getJSON(urlBandsintown2,
+		function(result){
+			console.log('search bandsintown2')
+			console.log(result);
+			if(result.length === 0){
+				console.log('no result from bandsintown2');
+				return;
+			};
+		})
+	return;
+}
 var searchSpotify = function(searchQuery, searchType, searchLimit){
 	var request = {
 		q: searchQuery,
@@ -57,16 +161,7 @@ var searchSpotify = function(searchQuery, searchType, searchLimit){
 		data: request,
 		type: 'json',
 	}
-	switch(searchType){
-		case 'artist':
-			searchArtist(settings);
-			break;
-		case 'album':
-			searchAlbum(settings);
-			break;
-		default:
-			searchTracks(settings);
-	}
+	searchTracks(settings);
 	return;
 }
 
@@ -107,7 +202,6 @@ $(document).ready( function() {
 		$('.result-body').show();
 		$('.result').show();
 		$('.detail-result').hide();
-
 		// zero out results if previous search has run
 		// !!! need to change this later !!!
 		$('.spotify-wrapper').html('');
@@ -117,25 +211,34 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		$('nav .text-input').attr('value',tags);
     searchSpotify(tags,'track', searchMax);
-
-
 		// $('.search-spotify')[0].reset();
 	});
 	$(document).on('click','.tracks-result',function(event){
-		var nameStr = $(this).find('.display-text1').text().split(': ', 2)[1];
-		var singerStr = $(this).find('.display-text2').text().split(': ', 2)[1];
+		var nameStr = $(this).find('.display-text1').text().split(': ')[1];
+		var singerStr = $(this).find('.display-text2').text().split(': ')[1];
 		var detailArr = $(this).find('.display-text3').text().split('_');
 		// console.log(nameStr);
 		// console.log(singerStr);
 		console.log(detailArr);
-		$('.detail-result .spotify-wrapper').append(displaySpotifyDetails(nameStr,singerStr,detailArr))
-		getMM(nameStr,singerStr);
-		// getTK(nameStr,singerStr);
-		// getBT(nameStr,singerStr);
+		$('.detail-result .spotify-wrapper').append(displaySpotifyDetails(nameStr,singerStr,detailArr));
+		$('.detail-result .songAndSingerNames').text(nameStr + '::' + singerStr);
 		$('.detail-result').show();
-
 	})
-
+  $(document).on('click','.extend-wrapper .mm-wrapper',function(event){
+		var nameStr = $(this).find('.songAndSingerNames').text().split('::')[0];
+		var singerStr = $(this).find('.songAndSingerNames').text().split('::')[1];
+		getMM(nameStr,singerStr);
+	})
+	$(document).on('click','.extend-wrapper .tk-wrapper',function(event){
+		var nameStr = $(this).find('.songAndSingerNames').text().split('::')[0];
+		var singerStr = $(this).find('.songAndSingerNames').text().split('::')[1];
+		getTK(nameStr,singerStr);
+	})
+	$(document).on('click','.extend-wrapper .bt-wrapper',function(event){
+			var nameStr = $(this).find('.songAndSingerNames').text().split('::')[0];
+			var singerStr = $(this).find('.songAndSingerNames').text().split('::')[1];
+			getBT(nameStr,singerStr);
+		})
 	$('.logo-small').on('click',function(){
 		$('.result-body').hide();
 		$('.result').hide();
@@ -144,81 +247,10 @@ $(document).ready( function() {
 	});
 });
 
-var getMM = function(name,artist){
-	console.log(name + ' by ' + artist);
-	$.ajax({
 
-			url: "http://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
-			//jsonp: false,
-			dataType: "jsonp",
-			jsonp: 'callback',
-			data: {
-							q_track: name,
-							q_artist: artist,
-							apikey: '33620d00ea8d8795ff4e49f0b4549d03',
-							format: 'jsonp',
-			}
-	}).done(function(result){
-		console.log('searched musizmatch');
-		console.log(result);
-		if(result.message.body.length === 0){
-			console.log('no result from musizmatch');
-			return;
-		};
-		// console.log(result.message.body.length);
-		console.log(result.message.body.lyrics.lyrics_id);
-		console.log(result.message.body.lyrics.lyrics_body);
-	});
-	return;
-};
-var getTK = function(name,artist){
-		$.ajax({
-				url: "https://www.tastekid.com/api/similar",
-		    //jsonp: false,
-		    dataType: "jsonp",
-				jsonp: 'callback',
 
-		    data: {
-								k: '240254-BingDai-6SQWHIFJ',
-								q: artist,
-								info: 1,
-								type: 'music',
-								format: 'jsonp',
-		    }
-		}).done(function(result){
-			console.log('searched tastekid');
-			console.log(result);
-			if(result.length === 0){
-				console.log('no result from tastekid');
-				return
-			};
-			// console.log(result.Similar.Info[0]);
-		});
-}
 
-var getBT = function(name,artist){
-	var urlBandsintown1 = 'http://api.bandsintown.com/artists/' + artist + '/events.json?api_version=2.0&app_id=spotifisearch&callback=?';
-	$.getJSON(urlBandsintown1,
-		function(result){
-			console.log('search bandsintown1')
-			console.log(result);
-			if(result.length === 0){
-				console.log('no result from bandsintown1');
-				return
-			};
-		})
-	var urlBandsintown2 = 'http://api.bandsintown.com/artists/'+ artist +'/events/search?format=json&app_id=spotifisearch&api_version=2.0&location=use_geoip&radius=150&callback=?'
-	$.getJSON(urlBandsintown2,
-		function(result){
-			console.log('search bandsintown2')
-			console.log(result);
-			if(result.length === 0){
-				console.log('no result from bandsintown2');
-				return;
-			};
-		})
-	return;
-}
+
 
 
 
